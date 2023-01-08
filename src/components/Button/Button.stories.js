1cf24a6a3ee7fe20e6181f8a557d8a538aa62046
Anurag Bansal <anuragbansaldps@gmail.com>
2023-01-08 15:26:33 +0530

    Decorators and arguments

--- src/components/Button/Button.stories.js
+++ src/components/Button/Button.stories.js
@@ -1,9 +1,11 @@
 import React from "react";
+import Center from "../Center/Center";
 import Button from "./Button";
 
 export default {
   title: "Form/Button",
   component: Button,
+  decorators: [(story) => <Center>{story()}</Center>],
 };
 
 export const Primary = () => <Button variant="primary">Primary</Button>;
@@ -11,16 +13,16 @@ export const Secondary = () => <Button variant="secondary">Secondary</Button>;
 export const Success = () => <Button variant="success">Success</Button>;
 export const Danger = () => <Button variant="danger">Danger</Button>;
 
-const Template = (args) => <Button {...args} />;
+// const Template = (args) => <Button {...args} />;
 
-export const PrimaryA = Template.bind({});
-PrimaryA.args = {
-  variant: "primary",
-  children: "Primary Args",
-};
+// export const PrimaryA = Template.bind({});
+// PrimaryA.args = {
+//   variant: "primary",
+//   children: "Primary Args",
+// };
 
-export const SecondaryA = Template.bind({});
-SecondaryA.args = {
-  variant: "secondary",
-  children: "Secondary Args",
-};
+// export const SecondaryA = Template.bind({});
+// SecondaryA.args = {
+//   variant: "secondary",
+//   children: "Secondary Args",
+// };
